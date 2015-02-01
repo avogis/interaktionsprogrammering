8d4c940b099b171c63c97a84d4ddf6407d03aa31
avogis <sigova@kth.se>
2015-02-01 15:38:55 +0100

    The meny page looks a bit more like it should

--- js/view/viewAllDishes.js
+++ js/view/viewAllDishes.js
@@ -2,27 +2,55 @@ var ViewAllDishes = function (container) {
 
     var model = new DinnerModel();
     var menu = model.getFullMenu();
-    var text = "";
 
-    this.names = container.find("#dinnerName");
-	    var myTableDiv = document.getElementById("dinnerDishes");
-	    var table = document.createElement('TABLE');
-	    var tableBody = document.createElement('TBODY');
+    var myTableDiv = document.getElementById("dinnerDishes");
+    var table = document.createElement('TABLE');
+    table.id = "menu";
+    var tableBody = document.createElement('TBODY');
 
-	    table.border = '1';
-	    table.appendChild(tableBody);
+    table.border = '1';
+    table.appendChild(tableBody);
 
-	    //TABLE ROWS
-		for (i = 0; i < menu.length; i++) {
-		    var tr = document.createElement('TR');
-		    for (j = 0; j < menu[i].length; j++) {
-		        var td = document.createElement('TD')
-		        td.appendChild(document.createTextNode(menu[i][j]));
-		        tr.appendChild(td)
+    var options = document.getElementById("chooseFood");
+    var optionNamesTemp = [];
+
+	for (i = 0; i < menu.length; i++) {
+	    var tr = document.createElement('TR');
+	    for (j = 0; j < menu[i].length; j++) {
+	    	if(i == 2){
+	    		var td = document.createElement('TD');
+	    		var img = document.createElement('IMG');
+	    		img.id = "foodPic";
+	    		img.src = "images/"+menu[i][j];
+	    		td.appendChild(img);
+		        tr.appendChild(td);
 		    }
-		    tableBody.appendChild(tr);
-		}
-
-	    myTableDiv.appendChild(table)
+	    	if(i == 0){
+				var td = document.createElement('TD');
+		        td.id = "foodName"
+		        td.appendChild(document.createTextNode(menu[i][j]));
+		        tr.appendChild(td);
+	    	}
+	    	if(i == 1){ //i don't understand why this isn't working
+	    		var found = optionNamesTemp.indexOf(menu[i][j]);
+	    		//alert("optionNamesTemp: " + optionNamesTemp + "\nfound: "+found + "\noptionNamesTemp.indexOf(menu[i][j]): "+optionNamesTemp.indexOf(menu[i][j])+"\n menu[i][j] "+menu[i][j]);
+				if(found == -1){
+					var option = document.createElement("option");
+					option.value = menu[i][j];
+					option.text = menu[i][j];
+					options.appendChild(option);
+					optionNamesTemp.push(menu[i][j].key);
+				}
+			}
+	    	if(i==3){
+    			var td = document.createElement('TD');
+    			td.id = "foodDescription";
+		        td.appendChild(document.createTextNode(menu[i][j]));
+		        tr.appendChild(td);
+		    }
+	    }
+	    tableBody.appendChild(tr);
+	}
+    myTableDiv.appendChild(table);
 
 } 
