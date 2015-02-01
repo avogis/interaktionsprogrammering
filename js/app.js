5f762048ea7b9db2d97120105beea8075285535b
Miranda <mirandarost@Mirandas-MacBook-Pro.local>
2015-02-01 12:08:25 +0100

    Started on a start page

--- js/app.js
+++ js/app.js
@@ -5,5 +5,6 @@ $(function() {
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"));
 	var viewAllDishes = new ViewAllDishes($("#dinnerDishes"));
+	var viewSTartPage = new ViewStartPage$("#viewStartPage"));
 
 });
---
+++ js/view/viewStartPage.js
@@ -0,0 +1,4 @@
+var ViewStartPage = function (container) {
+
+	
+}
