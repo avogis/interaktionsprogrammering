bb6163cc87a3102d61ce1a0bb267188d143c02aa
avogis <sigova@kth.se>
2015-02-21 18:07:20 +0100

    downloaded libs in order to work offline

--- js/controllers/dinnerBorderController.js
+++ js/controllers/dinnerBorderController.js
@@ -2,6 +2,8 @@ var DinnerBorderController = function(view, model) {
 	
 	view.goBackBtn.addEventListener("click",
 	   	function(e){
+	   		$("#spinner").addClass("hidden");
+        	$("#wait").addClass("hidden");
    			document.getElementById("viewAllDishes").style.display = "";
             document.getElementById("viewDinnerOverview").style.display = "none";
             document.getElementById("overviewBorder").style.display = "none";
